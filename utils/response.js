e95c8f08f4169bd1f3434b69b0ee368de8a10965
Soundarya.SU <Soundarya.SU@eNoah.CHN>
2026-01-29 16:22:44 +0530

    Response chnages

--- utils/response.js
+++ utils/response.js
@@ -1,4 +1,10 @@
 export default (statusCode, body) => ({
   statusCode,
+  headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*", 
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE"
+    },
   body: JSON.stringify(body),
 });
